a005a0fcd856c26568bccbd7fe2fec00b6d75d48
Thanuja <thanujalucky26@gmail.com>
2024-07-04 00:31:34 +0530

    Final Commit

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
       <footer className="footer footer-center bg-base-200 text-base-content rounded p-10 dark:bg-slate-900 dark:text-white">
   <nav className="grid grid-flow-col gap-4">
     <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
+    <a className="link link-hover" href='/contact'>Contact</a>
     <a className="link link-hover">Jobs</a>
     <a className="link link-hover">Press kit</a>
   </nav>
